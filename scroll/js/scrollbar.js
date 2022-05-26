839ae7ba3a876d998c02c064c411f5791665dc95
chihiro.tanigawa <chihiro.tanigawa@accenture.com>
2022-05-27 02:51:57 +0900

    なぜ

--- scroll/js/scrollbar.js
+++ scroll/js/scrollbar.js
@@ -1,6 +1,13 @@
-let $progress = document.querySelector('#progressbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
+const $progress = document.querySelector('#progressbar');
+//コンテンツの高さ(ウィンドウ外も含む)
+const $mainHeight = document.querySelector('.main');
+// const contentHeight = document.body.scrollHeight;
+const contentHeight = $mainHeight.offsetHeight;
+//はみ出た分を動かす
+//contenHeightが3651やったら良い(コンテンツの高さ)今はなぜか6000??くらいになる
+const totalHeight = contentHeight - window.innerHeight;
 window.onscroll = () => {
-  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  const progressHeight = (window.pageYOffset / totalHeight) * 100;
   $progress.style.height = progressHeight + "%";
+  console.log(contentHeight);
 }
